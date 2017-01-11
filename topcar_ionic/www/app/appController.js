1a218f619ca6abcee40f4ef3a9fb58ebf174ea79
Vinicius Audibert <vinicius.audibert@gmail.com>
2017-01-11 16:13:57 -0200

    feat(appjs): add function to change page

--- topcar_ionic/www/app/appController.js
+++ topcar_ionic/www/app/appController.js
@@ -1,6 +1,6 @@
 angular.module('app', [])
 
-.controller('appController', function($scope, $ionicModal, $timeout) {
+.controller('appController', function($scope, $ionicModal, $timeout, $state, $ionicHistory) {
   $scope.menuItems = [
     {name: 'Inicio', urlName: 'home'},
     {name: 'Catálogo', urlName: 'catalog'},
@@ -9,4 +9,11 @@ angular.module('app', [])
     {name: 'Serviços', urlName: 'services'},
     {name: 'Contato', urlName: 'contact'}
   ];
+  $scope.goTo = function (path, isBack) {
+    $ionicHistory.nextViewOptions({
+      disableBack: typeof(isBack) === 'undefined' ? false : isBack
+    });
+
+    $state.go(path);
+  };
 });
